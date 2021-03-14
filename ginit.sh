4dc9a141bdb3599b5b3a00ba934d473ea6ab9deb
Devops <dvps1239@gmail.com>
2021-03-14 22:47:19 +0300

    Add init gcloud command :wq

---
+++ ginit.sh
@@ -0,0 +1,1 @@
+gcloud compute instances create --boot-disk-size=10GB  image=ubuntu-1604-xenial-v20210224 --image-project=ubuntu-os-cloud --machine-type=g1-small --tags scriptserver5 --restart-on-failure --zone=europe-west1-d 
